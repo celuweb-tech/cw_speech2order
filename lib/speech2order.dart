8ebcfb39263de8ce9db3cb655e9cf4791735efa3
victormanuelgarciahurtado <vmgarciahurtado@gmail.com>
2024-10-30 12:02:17 -0500

    Add barrel file

---
+++ lib/speech2order.dart
@@ -0,0 +1,6 @@
+export 'package:cw_speech2order/main.dart';
+export 'package:cw_speech2order/model.dart';
+export 'package:cw_speech2order/proccess_speech.dart';
+export 'package:cw_speech2order/select_products_dialog.dart';
+export 'package:speech_to_text/speech_recognition_result.dart';
+export 'package:speech_to_text/speech_to_text.dart';
