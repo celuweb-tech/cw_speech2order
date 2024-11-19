686cbb53211e0a6c5638e180b1f8058319efa4b5
victor.garcia <victor.garcia@celuweb.com>
2024-11-19 00:52:07 -0500

    Add project example

--- lib/model.dart
+++ lib/model.dart
@@ -2,20 +2,18 @@ class Speech2OrderProduct {
   final String title;
   final String barCode;
   final String? quantity;
-  final String? unitQuantity;
 
-  Speech2OrderProduct(
-      {required this.title,
-      required this.barCode,
-      this.quantity,
-      this.unitQuantity});
+  Speech2OrderProduct({
+    required this.title,
+    required this.barCode,
+    this.quantity,
+  });
 
   factory Speech2OrderProduct.fromJson(Map<String, dynamic> json) {
     return Speech2OrderProduct(
-      title: json['titulo'],
-      barCode: json['cod_barras'],
-      quantity: json['cantidad'],
-      unitQuantity: json['cantidad_de_unidades'],
+      title: json['title'],
+      barCode: json['bar_code'],
+      quantity: json['quantity'],
     );
   }
 }
